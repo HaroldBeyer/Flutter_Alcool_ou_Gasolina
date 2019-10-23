7b6c2ffba3d3508c6b9b65e47996f58ceedeadee
Haroldo Beyer <haroldobeyerbacellar@gmail.com>
2019-10-22 22:49:00 -0300

    Calculando

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
-import 'package:alcool_ou_gasolina/widgets/input.widget.dart';
-import 'package:alcool_ou_gasolina/widgets/loading-button.widget.dart';
-import 'package:alcool_ou_gasolina/widgets/logo.widget.dart';
-import 'package:alcool_ou_gasolina/widgets/submit-form.dart';
-import 'package:alcool_ou_gasolina/widgets/sucess.widget.dart';
+import 'package:alcool_ou_gasolina/pages/home.page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_masked_text/flutter_masked_text.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,28 +14,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepPurple,
       ),
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  var gasCtrl = new MoneyMaskedTextController();
-  var alcCtrl = new MoneyMaskedTextController();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
-      body: ListView(
-        children: <Widget>[
-          Logo(),
-          SubmitForm(
-            alcCtrl: alcCtrl,
-            gasCtrl: gasCtrl,
-            submitFunc: () {},
-            busy: false,
-          ),
-        ],
-      ),
     );
   }
 }
---
+++ lib/pages/home.page.dart
@@ -0,0 +1,59 @@
+import 'package:alcool_ou_gasolina/widgets/logo.widget.dart';
+import 'package:alcool_ou_gasolina/widgets/submit-form.dart';
+import 'package:alcool_ou_gasolina/widgets/sucess.widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var _gasCtrl = new MoneyMaskedTextController();
+
+  var _alcCtrl = new MoneyMaskedTextController();
+
+  var _busy = false;
+
+  var _completed = false;
+
+  var _resultText = "Compensa utilizar álcool";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: ListView(
+        children: <Widget>[
+          Logo(),
+          _completed
+              ? Success(
+                  reset: () {},
+                  result: _resultText,
+                )
+              : SubmitForm(
+                  alcCtrl: _alcCtrl,
+                  gasCtrl: _gasCtrl,
+                  submitFunc: calculate,
+                  busy: _busy,
+                ),
+        ],
+      ),
+    );
+  }
+
+  Future calculate() {
+    // converte pra double, troca as virgulas por ponto
+    double alc =
+        double.parse(_alcCtrl.text.replaceAll(new RegExp(r'[,.]'), '')) / 100;
+    double gas =
+        double.parse(_gasCtrl.text.replaceAll(new RegExp(r'[,.]'), '')) / 100;
+    double res = alc / gas;
+
+    setState(() {
+      _completed = false;
+      _busy = true;
+    });
+  }
+}
